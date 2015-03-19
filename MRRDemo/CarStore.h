f0732c35418eb3cad68f980a0b25b3d6412ea4d3
cachenew <krunal@MacBookPro.local>
2015-03-19 15:26:31 +0530

    some modifications

---
+++ MRRDemo/CarStore.h
@@ -0,0 +1,13 @@
+//
+//  CarStore.h
+//  MRRDemo
+//
+//  Created by Krunal on 3/19/15.
+//  Copyright (c) 2015 KM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CarStore : NSObject
+
+@end
---
+++ MRRDemo/CarStore.m
@@ -0,0 +1,13 @@
+//
+//  CarStore.m
+//  MRRDemo
+//
+//  Created by Krunal on 3/19/15.
+//  Copyright (c) 2015 KM. All rights reserved.
+//
+
+#import "CarStore.h"
+
+@implementation CarStore
+
+@end
