f276b493ba967a6b2f2f308af16bbbc8fcd9c56e
cachenew <krunal@MacBookPro.local>
2015-03-19 14:35:31 +0530

    Initial Commit

---
+++ MRRDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MRRDemo
+//
+//  Created by Krunal on 3/19/15.
+//  Copyright (c) 2015 KM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MRRDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MRRDemo
+//
+//  Created by Krunal on 3/19/15.
+//  Copyright (c) 2015 KM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
